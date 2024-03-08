532164da4ce75b2b30882ac9082ab98d16ac2d1e
Harsh <harshvermajpr1@gmail.com>
2024-03-08 11:35:30 +0530

    Update Program_3.java

--- Program_3.java
+++ Program_3.java
@@ -2,52 +2,61 @@ import java.util.*;
 
 public class Main {
 
-    public static void n_No_Sum(int a) {
+    public static void n_No_Sum() {
         int sum = 0;
-        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= a; i++) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter total no. of digits here: ");
+        int b = sc.nextInt();
+
+        for (int i = 1; i <= b; i++) {
             System.out.println("Enter no " + i + " here: ");
             int num = sc.nextInt();
             sum = sum + num;
         }
-        System.out.println("Sum of " + a + " digits is: " + sum);
+        System.out.println("Sum of " +b + " digits is: " + sum);
+        sc.close();
 
 
     }
 
-    public static void n_No_Product(int a) {
+    public static void n_No_Product() {
         int product = 1;
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter total no. of digits here: ");
+        int b = sc.nextInt();
 
-        for (int i = 1; i <= a; i++) {
+        for (int i = 1; i <= b; i++) {
             System.out.println("Enter no " + i + " here: ");
             int num = sc.nextInt();
             product = product * num;
+
         }
-        System.out.println("Sum of " + a + " digits is: " + product);
+        System.out.println("Sum of " + b + " digits is: " + product);
+        sc.close();
 
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int ch = 1;
-        while (ch == 1) {
+        String ch = "y";
+        String pass;
+        while (ch.equals("y")) {
             System.out.print("Press 1 for Sum.\nPress 2 for Product.\nEnter your choice here: ");
             int choice = sc.nextInt();
-            System.out.print("Enter total no. of digits here: ");
-            int a = sc.nextInt();
 
             if (choice == 1) {
-                n_No_Sum(a);
+                n_No_Sum();
             } else if (choice == 2) {
-                n_No_Product(a);
+                n_No_Product();
             } else {
                 System.out.println("Invalid Choice.");
             }
-            System.out.print("Do you want to repeat ? 1 for yes || 0 for no : ");
-            ch = sc.nextInt();
-        }
+            System.out.print("Do you want to repeat ? yes or no : ");
+            ch = sc.next();
+            
+            System.out.println("You chose exit");
+        }sc.close();
         
 
     }
